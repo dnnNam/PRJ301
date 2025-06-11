0d01f6325182ea886f42fa547079d2e4bf8bf988
dainamnguyen3@gmail.com <dainamnguyen3@gmail.com>
2025-06-11 15:34:23 +0700

    MVC2 breakDown create DispatchServlet is a coordinating component

--- MVC2SE180138/src/java/namnd/util/DBHelper.java
+++ MVC2SE180138/src/java/namnd/util/DBHelper.java
@@ -18,16 +18,7 @@ public class DBHelper {
     
     public static Connection makeConnection ()
     throws ClassNotFoundException , SQLException{
-        
-        //1 load driver
-       Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-       // 2 Create connection string to connect container 
-       String url = "jdbc:sqlserver://"
-               + "localhost:1433;"
-               + "databaseName=PRJ301SE1809";
-       // 3 open connect driver manager
-       Connection con = DriverManager.getConnection(url , "sa" , "12345");
-       return con;
+       
        
     }
 }
